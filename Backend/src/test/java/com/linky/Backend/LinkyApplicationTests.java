a5196b90b92a3c840003a9cbf839745d83c1c8c0
Fabo011 <based011@protonmail.com>
2022-08-23 17:56:46 +0200

    Very basic setup with linky Frontend and Backend.

---
+++ Backend/src/test/java/com/linky/Backend/LinkyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linky.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
