feb105a0980d1bdf411dcb7377d9dc89e241fd96
Fabo011 <based011@protonmail.com>
2022-10-19 18:09:46 +0200

    Basic java spring backend.

--- Backend/src/config/configuration.ts
+++
@@ -1,9 +0,0 @@
-
-export default () => ({
-    port: parseInt(process.env.PORT, 10) || 3001,
-    /*database: {
-      host: process.env.DATABASE_HOST,
-      port: parseInt(process.env.DATABASE_PORT, 10) || 5432
-    }*/
-  });
-  
