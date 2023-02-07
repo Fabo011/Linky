45f8042151c065c16a2a14e2b48d9146270880fe
Fabo011 <based011@protonmail.com>
2023-02-07 21:08:50 +0100

    credentials changed.

--- Frontend/src/globalVariables.ts
+++ Frontend/src/globalVariables.ts
@@ -1,8 +1,7 @@
 const globalVaribales: string[]=
 [
     // baseBackendUrl
-    "https://www.threesixty-webdevelopers.com/"
-    //"https://linky-backend.onrender.com/"
+    "https://linky-backend.onrender.com/"
     //"http://localhost:3000/",
     
 ]
