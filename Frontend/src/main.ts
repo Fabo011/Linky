4fce1cb5081c28c61d08fe3f487d8862151a4791
Fabo011 <based011@protonmail.com>
2023-02-14 19:39:39 +0100

    bugs removed.

--- Frontend/src/main.ts
+++ Frontend/src/main.ts
@@ -1,7 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import { createApp } from 'vue'
 import App from './App.vue'
-import './registerServiceWorker'
 import router from './router'
 import "bootstrap/dist/js/bootstrap.js"
 import axios from 'axios'
