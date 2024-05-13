c7c398ac9da0b5cad1b8b50c2e973f26b78e514d
Fabo011 <based011@protonmail.com>
2024-05-13 20:25:15 +0200

    contact copy added.

--- src/components/userprofile/link/linkchildcomponents/TheRetrieveContacts.vue
+++ src/components/userprofile/link/linkchildcomponents/TheRetrieveContacts.vue
@@ -3,15 +3,24 @@
     <hr />
     <h6>Contact Informations</h6>
     <div v-if="item.contactname">
-      <p class="card-text"><i class="bi bi-person icons"></i>{{ item.contactname }}</p>
+      <p class="card-text">
+        <i class="bi bi-person icons"></i>{{ item.contactname }}
+        <i class="bi bi-clipboard copycontact" @click.prevent="copyContactName(item)"></i>
+      </p>
     </div>
 
     <div v-if="item.contactemail">
-      <p class="card-text"><i class="bi bi-envelope-at icons"></i>{{ item.contactemail }}</p>
+      <p class="card-text">
+        <i class="bi bi-envelope-at icons"></i>{{ item.contactemail }}
+        <i class="bi bi-clipboard copycontact" @click.prevent="copyContactEmail(item)"></i>
+      </p>
     </div>
 
     <div v-if="item.contactphonenumber">
-      <p class="card-text"><i class="bi bi-telephone icons"></i>{{ item.contactphonenumber }}</p>
+      <p class="card-text">
+        <i class="bi bi-telephone icons"></i>{{ item.contactphonenumber }}
+        <i class="bi bi-clipboard copycontact" @click.prevent="copyContactPhoneNumber(item)"></i>
+      </p>
     </div>
     <hr />
   </div>
@@ -19,6 +28,8 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { copiedtoast } from '@/components/toasts/toasts';
+import Clipboard from 'clipboard';
 
 export default defineComponent({
   name: 'TheRetrieveContacts.vue',
@@ -29,10 +40,48 @@ export default defineComponent({
       required: true,
     },
   },
+
+  methods: {
+    copyContactName(item: any) {
+      const contactName = item.contactname;
+      // @ts-ignore
+      new Clipboard('.copycontact', {
+        text: () => {
+          return contactName;
+        },
+      });
+      copiedtoast();
+    },
+
+    copyContactEmail(item: any) {
+      const contactEmail = item.contactemail;
+      // @ts-ignore
+      new Clipboard('.copycontact', {
+        text: () => {
+          return contactEmail;
+        },
+      });
+      copiedtoast();
+    },
+
+    copyContactPhoneNumber(item: any) {
+      const contactPhoneNumber = item.contactphonenumber;
+      // @ts-ignore
+      new Clipboard('.copycontact', {
+        text: () => {
+          return contactPhoneNumber;
+        },
+      });
+      copiedtoast();
+    },
+  },
 });
 </script>
 <style scoped>
 .icons {
   margin-right: 3px;
+}
+.copycontact {
+  cursor: pointer;
 }
 </style>
