0ce00412527303e48f479a174b6d691d67b657c6
Fabo011 <based011@protonmail.com>
2026-01-06 15:49:34 +0100

    policies added. usb code added.

--- sql/tables/policies.sql
+++ sql/tables/policies.sql
@@ -5,6 +5,20 @@ using (
   (auth.uid() = user_id)
 );
 
+alter policy "Enable delete for authenticated users only"
+on "public"."link"
+to authenticated
+with check (
+  true
+);
+
+alter policy "Enable insert for users based on user_id"
+on "public"."link"
+to public
+using (
+  (auth.uid() = user_id)
+);
+
 alter policy "Enable insert for authenticated users only"
 on "public"."link"
 to authenticated
@@ -19,11 +33,25 @@ using (
   (( SELECT auth.uid() AS uid) = user_id)
 );
 
+alter policy "Enable select for authenticated users only"
+on "public"."link"
+to authenticated
+with check (
+  true
+);
+
 alter policy "Enable update for users based on user_id"
 on "public"."link"
 to public
 using (
   (auth.uid() = user_id)
+);
+
+alter policy "Enable update for authenticated users only"
+on "public"."link"
+to authenticated
+with check (
+  true
 );
 
 create policy "Enable users to view their own data only"
